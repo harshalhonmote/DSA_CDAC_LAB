608753e0560fbf2a6a9022b2c7206eef2f59413f
Harshal Honmote <83832250+harshalhonmote@users.noreply.github.com>
2024-01-24 09:36:22 +0000

    map added

--- DSALAB/src/PlayGround.java
+++ DSALAB/src/PlayGround.java
@@ -70,5 +70,72 @@ public class PlayGround {
         System.out.println(d.toMillis());
         //-------------------------
         // return User.findAll().stream.map(UserDto::new).collect(toList());
+
+        //1.
+        Map<String, Long> counting = items.stream().collect(
+                Collectors.groupingBy(Item::getName, Collectors.counting()));
+        //o/p
+                //Group by + Count
+        // {
+        // 	papaya=1, banana=2, apple=3, orang=1, watermelon=1
+        // }
+       
+        //2.
+        Map<String, Integer> sum = items.stream().collect(
+                Collectors.groupingBy(Item::getName, Collectors.summingInt(Item::getQty)));
+        //Group by + Sum qty
+        // {
+        // 	papaya=20, banana=30, apple=40, orang=10, watermelon=10
+        // }
+       //COllectors.mapping() ->
+       //1.
+        Map<String, List<Integer>> result = personStream.collect(Collectors.groupingBy(p -> p.name, 
+                                                    Collectors.mapping(e -> e.age, Collectors.toList())));
+        //2.
+          Map<BigDecimal, Set<String>> result =
+                items.stream().collect(
+                        Collectors.groupingBy(Item::getPrice,
+                                Collectors.mapping(Item::getName, Collectors.toSet())
+                        )
+                );
+
+        //sort map by value--------------------------------------------------------
+        //https://mkyong.com/java/how-to-sort-a-map-in-java/
+        Map<String, Integer> sortedMap = sortByValue(unsortMap);
+        private static Map<String, Integer> sortByValue(Map<String, Integer> unsortMap) {
+
+        // 1. Convert Map to List of Map
+        List<Map.Entry<String, Integer>> list =
+                new LinkedList<Map.Entry<String, Integer>>(unsortMap.entrySet());
+
+        // 2. Sort list with Collections.sort(), provide a custom Comparator
+        //    Try switch the o1 o2 position for a different order
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2) {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        // 3. Loop the sorted list and put it into a new insertion order Map LinkedHashMap
+        Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+        for (Map.Entry<String, Integer> entry : list) {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+
+        /*
+        //classic iterator example
+        for (Iterator<Map.Entry<String, Integer>> it = list.iterator(); it.hasNext(); ) {
+            Map.Entry<String, Integer> entry = it.next();
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }*/
+        return sortedMap;
+    }//--------------------------------------------------------------------------
+
+
+
+
+
+
     }
 }
